7b6d501e9127e1293dc1eb42e82d595240af70ad
Dlussky Kirill <kirill@dlussky.ru>
2013-09-23 17:14:27 +0400

    Refactoring

--- README.txt
+++ README.txt
@@ -1,12 +1,58 @@
-Этот скрипт предназначен для использования сервиса гео-локации ipgeobase.ru 
+Этот скрипт предназначен для использования сервиса гео-локации ipgeobase.ru
 на PHP. Ipgeobase.ru предоставляет подробную информацию по IP-адресу: город,
 регион, федеральный округ, координаты - по городам России и Украины.
 По этим странам сервис работает точнее MaxMind GeoIP.
 
-1. Скачайте архив http://ipgeobase.ru/cgi-bin/Archive.cgi 
+Владислав Росс vladislav.ross@gmail.com
+
+=======================================
+
+В данном форке добавлена возможность выбирать кодировку выводимых данных,
+а также добавлен composer.json, удалены файлы с данными и произведен небольшой рефакторинг.
+
+Для пользователей composer:
+
+```
+"hundredminds/ipgeobase" : "1.02"
+```
+
+Чтобы не затруднять себя ручным обновлением файлов ipgeobase, рекомендуем настроить обновление через composer:
+
+1. в секцию "repositories" добавить:
+
+```
+    {
+        "type": "package",
+        "package": {
+            "name": "ipgeobase/data",
+            "version": "1.0",
+            "dist": {
+                "url": "http://ipgeobase.ru/files/db/Main/geo_files.zip",
+                "type": "zip"
+            }
+        }
+    }
+```
+2. в секцию "required":
+
+```
+"ipgeobase/data" : "1.0"
+```
+
+Теперь, когда захотите обновить файлы ipgeobase - достаточно будет увеличить номер версии в обеих секциях и запустить composer update.
+
+Разумеется, при этом придется передавать полный путь к файлам в конструктор:
+
+```
+$Gbase = new IPGeoBase(PROJECT_PATH . 'vendor/ipgeobase/data/cidr_optim.txt', PROJECT_PATH . 'vendor/ipgeobase/data/cities.txt');
+```
+
+=======================================
+
+Для тех, кто не использует Composer:
+
+1. Скачайте архив http://ipgeobase.ru/cgi-bin/Archive.cgi
    (хорошая идея настроить переодическое скачивание с помощью wget).
 2. Распакуйте cidr_optim.txt и cities.txt.
 2. Подключите ipgeobase.php.
 3. Используйте класс IPGeoBase (см. example.php).
-
-Владислав Росс vladislav.ross@gmail.com
--- ipgeobase.php
+++ ipgeobase.php
@@ -1,6 +1,7 @@
 <?php
 /*
-	Copyright 2013, Vladislav Ross
+    Copyright 2013, Vladislav Ross
+    Contributed by Timur Baymbetov, Kirill Dlussky.
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -14,103 +15,101 @@
 
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
-	
-*/
-class IPGeoBase 
-{
-	private $fhandleCIDR, $fhandleCities, $fSizeCIDR, $fsizeCities, $encoding;
-	function __construct($encoding = 'UTF-8', $CIDRFile = false, $CitiesFile = false)
-	{
-		if(!$CIDRFile)
-		{
-			$CIDRFile = dirname(__FILE__) . '/cidr_optim.txt';			
-		}
-		if(!$CitiesFile)
-		{
-			$CitiesFile = dirname(__FILE__) . '/cities.txt';			
-		}
-                $this->encoding = $encoding;
-		$this->fhandleCIDR = fopen($CIDRFile, 'r') or die("Cannot open $CIDRFile");
-		$this->fhandleCities = fopen($CitiesFile, 'r') or die("Cannot open $CitiesFile");
-		$this->fSizeCIDR = filesize($CIDRFile);
-		$this->fsizeCities = filesize($CitiesFile);
-	}
-	
-	private function getCityByIdx($idx)
-	{
-		rewind($this->fhandleCities);
-		while(!feof($this->fhandleCities))
-		{
-			$str = fgets($this->fhandleCities);
-			$arRecord = explode("\t", trim($str));
-			if($arRecord[0] == $idx)
-			{
-				return array_map(function($elem){
-					return iconv('windows-1251', $this->encoding, $elem);
-					}, ['city' => $arRecord[1],
-					  'region' => $arRecord[2],
-					  'district' => $arRecord[3]]) + 
-					 ['lat' => $arRecord[4],
-					  'lng' => $arRecord[5]];
-			}
-		}
-		return false;
-	}
-	
-	public function getRecord($ip)
-	{
-		$ip = sprintf('%u', ip2long($ip));
-		
-		rewind($this->fhandleCIDR);
-		$rad = floor($this->fSizeCIDR / 2);
-		$pos = $rad;
-		while(fseek($this->fhandleCIDR, $pos, SEEK_SET) != -1)			
-		{
-			if($rad) 
-			{
-				$str = fgets($this->fhandleCIDR);				
-			}
-			else
-			{
-				rewind($this->fhandleCIDR);
-			}
-			
-			$str = fgets($this->fhandleCIDR);
-			
-			if(!$str)
-			{
-				return false;
-			}
-			
-			$arRecord = explode("\t", trim($str));
 
-			$rad = floor($rad / 2);
-			if(!$rad && ($ip < $arRecord[0] || $ip > $arRecord[1]))
-			{
-				return false;
-			}
-			
-			if($ip < $arRecord[0])
-			{
-				$pos -= $rad;
-			}
-			elseif($ip > $arRecord[1])
-			{
-				$pos += $rad;
-			}
-			else
-			{
-				$result = array('range' => $arRecord[2], 'cc' => $arRecord[3]);
-											
-				if($arRecord[4] != '-' && $cityResult = $this->getCityByIdx($arRecord[4]))
-				{
-					$result += $cityResult;
-				}
-				
-				return $result;
-			}
-		}
-		return false;		
-	}
+*/
+class IPGeoBase
+{
+    private $fhandleCIDR, $fhandleCities, $fSizeCIDR, $fsizeCities, $outputEncoding;
+
+    const DEFAULT_FILE_ENCODING = 'windows-1251';
+    const DEFAULT_OUTPUT_ENCODING = 'UTF-8';
+
+    function __construct($CIDRFile = null, $CitiesFile = null, $outputEncoding = self::DEFAULT_OUTPUT_ENCODING)
+    {
+        $CIDRFile = $CIDRFile ?: dirname(__FILE__) . '/cidr_optim.txt';
+        $CitiesFile = $CitiesFile ?: dirname(__FILE__) . '/cities.txt';
+
+        $this->outputEncoding = $outputEncoding;
+
+        if (!file_exists($CIDRFile) ||
+            !($this->fhandleCIDR = fopen($CIDRFile, 'r'))) {
+            throw new \RuntimeException("Cannot access CIDR file: $CIDRFile");
+        }
+        if (!file_exists($CitiesFile) ||
+            !($this->fhandleCities = fopen($CitiesFile, 'r'))) {
+            throw new \RuntimeException("Cannot access cities file: $CitiesFile");
+        }
+
+        $this->fSizeCIDR = filesize($CIDRFile);
+        $this->fsizeCities = filesize($CitiesFile);
+    }
+
+    private function getCityByIdx($idx)
+    {
+        rewind($this->fhandleCities);
+
+        while (!feof($this->fhandleCities)) {
+            $str = fgets($this->fhandleCities);
+            $arRecord = explode("\t", trim($str));
+            if ($arRecord[0] == $idx) {
+                $outputConverter = function ($string) {
+                    return iconv(self::DEFAULT_FILE_ENCODING, $this->outputEncoding, $string);
+                };
+
+                return array_map($outputConverter, array(
+                    'city' => $arRecord[1],
+                    'region' => $arRecord[2],
+                    'district' => $arRecord[3]
+                )) + array(
+                    'lat' => $arRecord[4],
+                    'lng' => $arRecord[5]
+                );
+            }
+        }
+
+        return false;
+    }
+
+    public function getRecord($ip)
+    {
+        $ip = sprintf('%u', ip2long($ip));
+
+        rewind($this->fhandleCIDR);
+        $rad = floor($this->fSizeCIDR / 2);
+        $pos = $rad;
+        while (fseek($this->fhandleCIDR, $pos, SEEK_SET) != -1) {
+            if (!$rad) {
+                rewind($this->fhandleCIDR);
+            }
+
+            $str = fgets($this->fhandleCIDR);
+
+            if (!$str) {
+                return false;
+            }
+
+            $arRecord = explode("\t", trim($str));
+
+            $rad = floor($rad / 2);
+            if (!$rad && ($ip < $arRecord[0] || $ip > $arRecord[1])) {
+                return false;
+            }
+
+            if ($ip < $arRecord[0]) {
+                $pos -= $rad;
+            } elseif ($ip > $arRecord[1]) {
+                $pos += $rad;
+            } else {
+                $result = array('range' => $arRecord[2], 'cc' => $arRecord[3]);
+
+                if ($arRecord[4] != '-' && $cityResult = $this->getCityByIdx($arRecord[4])) {
+                    $result += $cityResult;
+                }
+
+                return $result;
+            }
+        }
+        return false;
+    }
 }
 
